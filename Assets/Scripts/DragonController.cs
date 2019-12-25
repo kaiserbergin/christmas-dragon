1d0faeb37d9128762ca3e12d3936c8445abc323e
Donovan Bergin <kaiserbergin@gmail.com>
2019-12-24 21:11:33 -0600

    Adding base project

---
+++ Assets/Scripts/DragonController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class DragonController : MonoBehaviour
+{
+    private Animator _animator;
+
+    private Vector2 movementInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = this.gameObject.GetComponent<Animator>();
+        foreach (var param in _animator.parameters)
+        {
+            Debug.Log(param.name + " :: " + param.type + " :: " + param.nameHash);
+        }
+        _animator.SetBool("Fly Idle", true);
+        _animator.SetBool("Fly Forward", true);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = movementInput.x;
+        float verticalInput = movementInput.y;
+    }
+
+    public void Move(InputAction.CallbackContext context) => movementInput = context.ReadValue<Vector2>();
+}
